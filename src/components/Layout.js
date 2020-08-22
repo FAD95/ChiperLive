7b87d40be5c93e3ccd13c23cc265a412f2be55d8
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-21 23:49:55 -0500

    Header modificado. (componentes aun con ejemplo de redux)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,41 +1,33 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import Link from 'next/link'
-
-export default class Layout extends PureComponent {
-  render () {
-    return (
-      <div className='layout'>
-        <header>
-          <img src='/static/next-logo.png' />
-          <h3>nextjs redux starter</h3>
-          <menu>
-            <Link href='/about'>
-              <a>About</a>
-            </Link>
-            <Link href='/'>
-              <a>Redux demo</a>
-            </Link>
-          </menu>
-          <style jsx>{`
-            header {
-              display: flex;
-              align-items: center;
-              font-family: Monospace;
-              margin: 10px 20px;
-            }
-            h3 {
-              margin-left: 10px
-            }
-            img {
-              width: 30px;
-            }
-            menu > a {
-              margin-right: 16px;
-            }
-          `}</style>
-        </header>
-        { this.props.children }
-      </div>
-    )
-  }
+const  Layout = memo(({children})=>{
+  return (
+    <div className='layout'>
+      <header>
+        <img src='/static/chiper-logo.svg' />
+        <h3>LIVE</h3>
+        { children }
+        <style jsx>{`
+          header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            font-family: Monospace;
+            margin: 10px 20px;
+          }
+          h3 {
+            color: red;
+            margin-left: 10px;
+            font-size: 3em;
+          }
+          img {
+            width: 100px;
+          }
+        `}</style>
+      </header>
+    </div>
+  )
 }
+)
+
+export default Layout
