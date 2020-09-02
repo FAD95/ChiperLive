966d6febb27408dcf99a97f93f963fb32df89d58
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-02 01:30:40 -0500

    evento onClick

--- Client/src/components/Button.js
+++ Client/src/components/Button.js
@@ -1,24 +1,29 @@
 import Link from 'next/link'
 
-const Button = ({ width, height, link, children }) => {
+const Button = ({ width, height, link, onClick, children }) => {
   return (
     <>
-      <Link href={link}>
-        <button>{children}</button>
-      </Link>
-      <style jsx>{`
-            button {
-                background-color: #13ce66;
-                color: white;
-                width: ${width};
-                height: ${height};
-                border-radius: 10px;
-                border: none;
-                font-weight: bold;
-                font-size:1em;
-                padding: 10px 20px;
-                text-decoration: none;
-            }
+      {link ? (
+        <Link href={link}>
+          <button onClick={onClick}>{children}</button>
+        </Link>
+      ) : (
+        <button onClick={onClick}>{children}</button>
+      )}
+      <style jsx>
+        {`
+          button {
+            background-color: #13ce66;
+            color: white;
+            width: ${width};
+            height: ${height};
+            border-radius: 10px;
+            border: none;
+            font-weight: bold;
+            font-size: 1em;
+            padding: 10px 20px;
+            text-decoration: none;
+          }
         `}
       </style>
     </>
