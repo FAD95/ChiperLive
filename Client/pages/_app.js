1878e13b0f097cb776192b55bd7010168315ba8b
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-02 01:35:06 -0500

    configuracion store y Provider de redux

--- Client/pages/_app.js
+++ Client/pages/_app.js
@@ -1,11 +1,13 @@
 import '../src/styles/GlobalStyles.css'
 import Layout from '../src/containers/Layout'
+import { Provider } from 'react-redux'
+import store from '../src/redux/store'
 
-export default function App ({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Layout />
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
