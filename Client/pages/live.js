8458a81ff73e98b1c8b88fa045e844c63e394c4f
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-02 02:21:49 -0500

    Live

--- Client/pages/live.js
+++ Client/pages/live.js
@@ -1,15 +1,83 @@
+import { useState } from 'react'
 import Head from '../src/components/head'
+import Button from '../src/components/Button'
+import Video from '../src/components/Video'
+import { startMaster, stopMaster } from '../src/kinesis/master'
+import { connect } from 'react-redux'
+import addChannelName from '../src/redux/actions/addChannelName'
+import removeChannelName from '../src/redux/actions/removeChannelName'
 import useLiveStream from '../src/hooks/useLiveStream'
 import useLocalStream from '../src/hooks/useLocalStream'
 
-export default function FirstPost () {
-  useLiveStream()
+// const formValuesExample = {
+//   region: formValues.region,
+//   accessKeyId: formValues.accessKeyId,
+//   secretAccessKey: formValues.secretAccessKey,
+//   sessionToken: formValues.sessionToken,
+//   endpoint: formValues.endpoint,
+//   channelName: 'Prueba1',
+// }
+
+const formValues = {
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACEESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  channelName: 'Prueba8',
+  sendVideo: true,
+  sendAudio: true,
+}
+
+function Live({ channelName, addChannelName, removeChannelName }) {
   useLocalStream()
+  useLiveStream(channelName, addChannelName, removeChannelName, formValues)
+  function onStatsReport(report) {
+    // TODO: Publish stats
+  }
+
+  const handleStopTransmission = async (e) => {
+    e.preventDefault()
+    stopMaster()
+  }
+  const handleStartTransmission = async (e) => {
+    e.preventDefault()
+    startMaster(formValues, onStatsReport, (event) => {
+      remoteMessage.append(`${event.data}\n`)
+    })
+  }
 
   return (
     <>
-      <Head title='Chiper Live || En vivo' />
-      <video autoPlay />
+      <Head title="Chiper Live || En vivo" />
+      <Video />
+      <Button
+        width="100px"
+        height="80px"
+        link=""
+        onClick={handleStopTransmission}
+      >
+        Parar transmisión
+      </Button>
+      <Button
+        width="100px"
+        height="80px"
+        link=""
+        onClick={handleStartTransmission}
+      >
+        Reanudar transmisión
+      </Button>
     </>
   )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    channelName: state.channelName,
+  }
+}
+
+const mapDispatchToProps = {
+  addChannelName,
+  removeChannelName,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Live)
