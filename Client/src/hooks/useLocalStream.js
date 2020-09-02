bc6a045cf3d4980519010fc435f48d149b23737d
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-02 01:27:29 -0500

    resolucion captada en getMediaUser

--- Client/src/hooks/useLocalStream.js
+++ Client/src/hooks/useLocalStream.js
@@ -1,15 +1,23 @@
 import { useEffect } from 'react'
+import isMobile from '../utils/isMobile'
 
 const useLocalStream = () => {
   useEffect(() => {
     let localStream
 
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-      .then(stream => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: isMobile()
+          ? { facingMode: 'user' }
+          : { width: { exact: 480 }, height: { exact: 640 } },
+        audio: false,
+      })
+      .then((stream) => {
         localStream = stream
         var video = document.querySelector('video')
         video.srcObject = stream
-      }).catch(err => {
+      })
+      .catch((err) => {
         console.error(err)
         return
       })
