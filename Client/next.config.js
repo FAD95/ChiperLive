8e0ac73b7f810a465b327a34db8acc2848616ce2
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-02 02:20:14 -0500

    configuracion de .env

--- Client/next.config.js
+++ Client/next.config.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
+
 module.exports = {
   webpack: (config) => {
     config.node = {
-      fs: 'empty'
+      fs: 'empty',
     }
     return config
-  }
+  },
+  env: {
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_ACEESS_KEY_ID: process.env.AWS_ACEESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 }
