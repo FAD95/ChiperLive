5badeacccfebd301e2bee47280534650f3784ceb
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-02 01:28:01 -0500

    implementado con kinesis

--- Client/src/hooks/useLiveStream.js
+++ Client/src/hooks/useLiveStream.js
@@ -1,85 +1,38 @@
 import { useEffect } from 'react'
-import io from 'socket.io-client'
+import createSignalingChannel from '../kinesis/createSignalingChannel'
+import deleteSignalingChannel from '../kinesis/deleteSignalingChannel'
+import { stopMaster } from '../kinesis/master'
 
-const useLiveStream = () => {
+const useLiveStream = (
+  channelName,
+  addChannelName,
+  removeChannelName,
+  formValues
+) => {
   useEffect(() => {
-    let liveStream = {}
-    const peers = {}
-
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-      .then(stream => {
-        liveStream = stream
-        const socket = io('http://localhost:8080/')
-
-        const broadcaster = {
-          room: 0,
-          name: 'Alejandro Zapata Molina'
+    if (channelName === '') {
+      // callback que se llama cuando se crea el channelARN dentro de createSignalingChannel
+      addChannelName(formValues.channelName)
+      const callCreateSignalingChannel = async () => {
+        try {
+          await createSignalingChannel(formValues)
+        } catch (error) {
+          console.error(error)
+          console.log('The channel already exists')
         }
-
-        socket.emit('new broadcaster', broadcaster.room)
-
-        socket.on('new viewer', viewer => {
-          peers[viewer.id] = new RTCPeerConnection({
-            iceServers: [
-              { urls: 'stun:stun.services.mozilla.com' },
-              { urls: 'stun:stun.l.google.com:19302' }
-            ]
-          })
-
-          stream.getTracks().forEach(track => {
-            peers[viewer.id].addTrack(track, stream)
-          })
-
-          peers[viewer.id].onicecandidate = event => {
-            if (event.candidate) {
-              socket.emit('candidate', viewer.id, {
-                type: 'candidate',
-                label: event.candidate.sdpMLineIndex,
-                id: event.candidate.sdpMid,
-                candidate: event.candidate.candidate
-              })
-            }
-          }
-
-          peers[viewer.id].createOffer()
-            .then(sessionDescription => {
-              peers[viewer.id].setLocalDescription(sessionDescription)
-
-              socket.emit('offer', viewer.id, {
-                type: 'offer',
-                sdp: sessionDescription,
-                broadcaster: broadcaster
-              })
-            })
-            .catch(err => {
-              console.error(err)
-            })
-        })
-
-        socket.on('answer', (viewerId, event) => {
-          peers[viewerId].setRemoteDescription(new RTCSessionDescription(event))
-        })
-
-        socket.on('candidate', function (id, event) {
-          console.log(event)
-          const candidate = new RTCIceCandidate({
-            sdpMLineIndex: event.label,
-            candidate: event.candidate
-          })
-          peers[id].addIceCandidate(candidate)
-        })
-      })
-      .catch(err => {
-        console.error(err)
-        return
-      })
-      return () => {
-        liveStream.getTracks().forEach((track) => {
-          track.stop()
-        })
       }
-  }, [])
- 
+      callCreateSignalingChannel()
+    }
+    return () => {
+      if (channelName !== '') {
+        alert('Se parara la transmision')
+        stopMaster()
+        deleteSignalingChannel(channelName)
+        removeChannelName()
+        console.log('Channel deleted')
+      }
+    }
+  }, [channelName])
 }
 
 export default useLiveStream
