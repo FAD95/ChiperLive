ffa74a80a0b44b625ea47f497499dbc4c21e79cf
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-09-03 18:26:17 -0500

    Implementing navigation

--- App/src/components/Menu.js
+++ App/src/components/Menu.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
-import { Image, StyleSheet } from 'react-native'
+import { Image, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
+import { DrawerNavigatorItems, createDrawerNavigator } from 'react-navigation-drawer';
+
+const RootStack = createDrawerNavigator({
+
+}, {contentComponent: props => <CustomDrawerComponent {...props}/>})
+
+const CustomDrawerComponent = (props) => {
+    <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+        <DrawerNavigatorItems {...props}/>
+    </SafeAreaView>
+}
+
 
 const Menu = () => {
+    
+
+
     return (
-        <Image style={styles.drawer} source={require('../img/drawer.png')} />
+        <TouchableOpacity onPress={}>
+            <Image style={styles.drawer} source={require('../img/drawer.png')} />
+        </TouchableOpacity>
     )
 }
 
