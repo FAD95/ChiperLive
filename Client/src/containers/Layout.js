f2e762280780f870c4b24c296e1b5bde9bbb4d6f
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-17 03:11:46 -0500

    [ADD] redux para leer el estado de isLive y prevenir el cierre inesperado de un Live

--- Client/src/containers/Layout.js
+++ Client/src/containers/Layout.js
@@ -1,20 +1,39 @@
 import { memo } from 'react'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import Menu from '../components/Menu'
 
+import { useSelector } from 'react-redux'
+import setIsLive from '../redux/actions/setIsLive'
+
 const Layout = memo(() => {
-  const loggedIn = true
+  const isLive = useSelector((store) => store.isLive)
+
+  const router = useRouter()
+  const finishLive = (e) => {
+    e.preventDefault()
+    if (isLive) {
+      const ans = confirm('Are you sure you want to finish the LIVE?')
+      if (ans) {
+        router.push('/')
+      } else {
+        console.log('Nothing happened')
+      }
+    } else {
+      router.push('/')
+    }
+  }
+
   return (
-    <div className="layout">
+    <div className='layout'>
       <header>
-        {loggedIn && <Menu />}
-        <Link href="/">
-          <a>
-            <img src="/images/chiper-logo.svg" />
-            <h3>LIVE</h3>
-          </a>
-        </Link>
+        <Menu />
+
+        <a onClick={(e) => finishLive(e)}>
+          <img src='/images/chiper-logo.svg' />
+          <h3>LIVE</h3>
+        </a>
+
         <div />
         <style jsx>
           {`
