eb1162a9efe1b628200fa19ba5c06d27c9e40240
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-17 03:07:27 -0500

    [ADD] react-loader-spinner css

--- Client/pages/_app.js
+++ Client/pages/_app.js
@@ -1,9 +1,10 @@
 import '../src/styles/GlobalStyles.css'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import Layout from '../src/containers/Layout'
 import { Provider } from 'react-redux'
 import store from '../src/redux/store'
 
-export default function App({ Component, pageProps }) {
+export default function App ({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Layout />
