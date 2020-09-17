09854ace20182200761e1a37736c2632524618e0
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-17 03:06:32 -0500

    linted

--- Client/next.config.js
+++ Client/next.config.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 module.exports = {
   webpack: (config) => {
     config.node = {
-      fs: 'empty',
+      fs: 'empty'
     }
     return config
   },
   env: {
     AWS_REGION: process.env.AWS_REGION,
     AWS_ACEESS_KEY_ID: process.env.AWS_ACEESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
+  }
 }
