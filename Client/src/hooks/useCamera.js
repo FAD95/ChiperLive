6c4fd00e86bc65ae00f0d6d94f2dd86f75b0aa10
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-17 03:13:54 -0500

    [ADD] hook que inicia la camara al iniciar la ruta de live

---
+++ Client/src/hooks/useCamera.js
@@ -0,0 +1,70 @@
+import { useEffect, useState, useRef } from 'react'
+import isMobile from '../utils/isMobile'
+
+const useCamera = () => {
+  const [cameraEnabled, setCameraEnabled] = useState(false)
+  const inputStreamRef = useRef()
+  const videoRef = useRef()
+  const canvasRef = useRef()
+  const requestAnimationRef = useRef()
+
+  const updateCanvas = () => {
+    if (
+      !videoRef.current ||
+      videoRef.current.ended ||
+      videoRef.current.paused
+    ) {
+      return
+    }
+    const ctx = canvasRef.current.getContext('2d')
+
+    ctx.drawImage(
+      videoRef.current,
+      0,
+      0,
+      videoRef.current.clientWidth,
+      videoRef.current.clientHeight
+    )
+
+    requestAnimationRef.current = requestAnimationFrame(updateCanvas)
+  }
+
+  useEffect(() => {
+    const reqCamera = async () => {
+      try {
+        inputStreamRef.current = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: { facingMode: 'user' }
+        })
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    reqCamera().then(async () => {
+      videoRef.current.srcObject = inputStreamRef.current
+
+      try {
+        await videoRef.current.play()
+        canvasRef.current.height = videoRef.current.clientHeight
+        canvasRef.current.width = videoRef.current.clientWidth
+        requestAnimationRef.current = requestAnimationFrame(updateCanvas)
+        setCameraEnabled(true)
+      } catch (error) {
+        console.error(error)
+      }
+    })
+
+    return () => {
+      inputStreamRef.current.getTracks().forEach((track) => {
+        track.stop()
+      })
+      setCameraEnabled(false)
+    }
+  }, [])
+  return [
+    { inputStreamRef, videoRef, canvasRef, requestAnimationRef, cameraEnabled }
+  ]
+}
+
+export default useCamera
