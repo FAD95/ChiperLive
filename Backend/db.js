e019d6ae21055524724f52068afed40f65337869
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-21 03:46:05 -0500

    [FIX] try catch

--- Backend/db.js
+++ Backend/db.js
@@ -1,19 +1,20 @@
-const db= require('mongoose');
+const db = require('mongoose')
 
-//mongodb+srv://dsanchez:Dsanchez56@diegokenwood-vmax7.mongodb.net/test?retryWrites=true&w=majority
-db.Promise = global.Promise;
+db.Promise = global.Promise
 
-async function connect (url){
-    await db.connect(url,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-        dbName:'chiperlive'
-      })
-
-    console.log('[db] Conectada con exito');
-      
+async function connect(url) {
+  try {
+    await db.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'chiperlive',
+    })
+    console.log('[db] Conectada con exito')
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-module.exports={
-    connect
+module.exports = {
+  connect,
 }
