8ef06322a8280768d586bef5388d9646a2b9679a
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-21 03:26:33 -0500

    [ADD] login promise

---
+++ Client/src/axios/login.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const ENDPOINT = process.env.SERVER
+
+const Login = ({ email, password }) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(ENDPOINT + '/login', {
+        email,
+        password,
+      })
+      .then(function (response) {
+        resolve(response)
+      })
+      .catch(function (error) {
+        if (error.response)
+          if (error.response.status === 401) {
+            reject('E-mail o contraseña incorrectos')
+          }
+        reject(error)
+      })
+  })
+}
+export default Login
