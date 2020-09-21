e198033c4c09125199e8bdf4655dcb48b822a19d
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-21 03:20:10 -0500

    [ADD]  Rutas loginMediaServices

---
+++ LiveServer/src/network/routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const loginMediaServices = require('../components/loginMediaServices/network')
+// const startStreaming = require('../components/startStreaming/network')
+const routes = function (server) {
+  server.use('/loginMediaServices', loginMediaServices)
+  // server.use('/startStreaming', startStreaming)
+}
+
+module.exports = routes
