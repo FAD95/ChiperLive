ef2b920af26247fff7f7dda3db519ca164857242
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-21 03:38:27 -0500

    linted

--- Backend/components/login/store.js
+++ Backend/components/login/store.js
@@ -1,39 +1,33 @@
+const Model = require('../user/model')
 
-const Model = require('../user/model');
-
-
- 
-function addUser(user){
-  const myUser = new Model(user);
-   return myUser.save();
+function addUser(user) {
+  const myUser = new Model(user)
+  return myUser.save()
 }
 
- async function getUser(filterUser){
-    let filter = {}; 
-  if (filterUser !== null){
-      filter = {"correo": filterUser }
-  } 
-    
-   const  users = await Model.findOne(filter,'correo password');
-   
-     
-    return users;
+async function getUser(filterUser) {
+  let filter = {}
+  if (filterUser !== null) {
+    filter = { email: filterUser }
   }
 
-  
-  async function updateUser(id,name){
-    const foundUser = await Model.findOne({
-      _id:id
-    })
+  const users = await Model.findOne(filter, 'correo password')
 
-    
-    foundUser.name = name;
-   
-    const UpdatedUser = await foundUser.save();
-    return UpdatedUser
-  }
-  
- /* function removeMessage(id){
+  return users
+}
+
+async function updateUser(id, name) {
+  const foundUser = await Model.findOne({
+    _id: id,
+  })
+
+  foundUser.name = name
+
+  const UpdatedUser = await foundUser.save()
+  return UpdatedUser
+}
+
+/* function removeMessage(id){
     const deleted = Model.deleteOne({
        _id :id
      })
@@ -41,9 +35,9 @@ function addUser(user){
      return deleted;
   }
   */
-module.exports= {
-    list :getUser,
-    addUser ,
-    update: updateUser
-    //delete
+module.exports = {
+  list: getUser,
+  addUser,
+  update: updateUser,
+  //delete
 }
