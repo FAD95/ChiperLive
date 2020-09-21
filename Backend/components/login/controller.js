965c08eb20fbf6c220387ceea2fe4739aa147762
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-21 03:39:22 -0500

    [FIX] variables  nombradas en ingles

--- Backend/components/login/controller.js
+++ Backend/components/login/controller.js
@@ -1,44 +1,43 @@
-const store = require('./store');
-
-
- 
+const store = require('./store')
 
 function getUser(filterUser) {
-  return new Promise((resolve, reject) => {
-    resolve(store.list(filterUser));
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await store.list(filterUser)
+      resolve(user)
+    } catch (error) {
+      reject('Usuario inexistente')
+      console.error(error)
+    }
   })
-
 }
 
-
 function addUser(user) {
-
   const users = {
-    "nombres": user.nombres,
-    "apellidos": user.apellidos,
-    "telefono": user.telefono,
-    "correo": user.correo,
-    "password": user.password,
-    "ciudad": user.ciudad,
-    "rol": user.rol
+    firstName: user.firstName,
+    lastName: user.lastName,
+    phone: user.phone,
+    email: user.email,
+    password: user.password,
+    city: user.city,
+    role: user.role,
   }
-  return store.addUser(users);
+  return store.addUser(users)
 }
 
 function updateUser(id, user) {
   return new Promise(async (resolve, reject) => {
     if (!id || !user) {
       reject('Invalid data')
-      return false;
+      return false
     } else {
-      const result = await resolve(store.update(id, user));
-      return result;
+      const result = await resolve(store.update(id, user))
+      return result
     }
   })
 }
 module.exports = {
   getUser,
   addUser,
-  updateUser 
-
+  updateUser,
 }
