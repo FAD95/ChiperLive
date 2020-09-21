14b6b16d2266fae7db76dd55169f7ca698fc5ea3
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-21 17:57:24 -0500

    [DEL]

--- Backend/network/routes.js
+++ Backend/network/routes.js
@@ -1,14 +1,10 @@
-const express =require('express');
-const message = require('../components/message/network')
+const express = require('express')
 const user = require('../components/user/network')
-const chat = require('../components/chat/network')
-const login = require ('../components/login/network')
-const routes = function(server){
-  
-  server.use('/login',login);
-  server.use('/user',user);
-  server.use('/chat',chat);
-  server.use('/singup',user);
+const login = require('../components/login/network')
+const routes = function (server) {
+  server.use('/login', login)
+  server.use('/user', user)
+  server.use('/singup', user)
 }
 
-module.exports = routes;
+module.exports = routes
