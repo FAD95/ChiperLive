7c5c3bf6efff02602c281775a9cb0c1bd9e51010
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-21 03:43:49 -0500

    [FIX] este servidor no necesita pasar port http

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,24 @@
-const express = require ('express');
-var app = express();
-const server =require('http').Server(app);
-const config =require('./config');
+const express = require('express')
+var app = express()
+const config = require('./config')
 
-const bodyparser = require('body-parser');
-const db = require('./db');
-const router =require('./network/routes')
+const bodyparser = require('body-parser')
+const db = require('./db')
+const router = require('./network/routes')
 const cors = require('cors')
 
-db.connect(config.dbUrl);
+db.connect(config.dbUrl)
 
-require ('./auth/auth')
+require('./auth/auth')
 
-app.use(cors());
-app.use(bodyparser.json());
+app.use(cors())
+app.use(bodyparser.json())
 
-router(app);
+router(app)
 
-
-app.use(config.publicRoute,express.static('public'));
-server.listen(config.port,function(){
-    console.log(`La aplicacion está escuchando en ${config.host}:${config.port}${config.publicRoute}`);
-
-});
-
+app.use(config.publicRoute, express.static('public'))
+app.listen(config.port, function () {
+  console.log(
+    `La aplicacion está escuchando en ${config.host}:${config.port}${config.publicRoute}`
+  )
+})
