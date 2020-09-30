4af8a271177336c61b19d8d5ec67d6137b5716a5
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-30 16:55:26 -0500

    [ADD] Variables de entorno

--- LiveServer/config.js
+++ LiveServer/config.js
@@ -6,6 +6,13 @@ const config = {
   publicRoute: process.env.PUBLIC_ROUTE || '/app',
   tokenCaducidad: process.env.TOKEN_CADUCIDAD || '2h',
   seedJwt: process.env.SEED_JWT || 'ChiperLive43ver',
+  azureSubscriptionId:
+    process.env.AZURE_SUBSCRIPTION_ID || 0000 - 0000 - 0000 - 0000,
+  azureResourceGroupName: process.env.AZURE_RESOURCE_GROUP_NAME || 'default',
+  azureAccountName: process.env.AZURE_ACCOUNT_NAME || 'default',
+  azureClientId: process.env.AZURE_CLIENT_ID || 'default',
+  azureClientSecret: process.env.AZURE_CLIENT_SECRET || 'default',
+  azureStorageAccountName: process.env.AZURE_STORAGE_ACCOUNT_NAME || 'default',
 }
 
 module.exports = config
