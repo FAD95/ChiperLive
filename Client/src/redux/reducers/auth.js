14d256ad6fee0f492edd82eb1dde09c5da1f9a1b
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-30 16:57:46 -0500

    [ADD] UserID

--- Client/src/redux/reducers/auth.js
+++ Client/src/redux/reducers/auth.js
@@ -3,6 +3,7 @@ import { type as SET_AUTH } from '../actions/setAuth'
 const defaultState = {
   status: false,
   token: '',
+  userID: '',
 }
 
 const auth = (state = defaultState, { type, payload }) => {
