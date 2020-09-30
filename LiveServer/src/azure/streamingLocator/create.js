67b73d804d00f099805ca2f09a6f7f4bcd01cd61
Fadith Escorcia <feescorciam@protonmail.com>
2020-09-30 16:50:24 -0500

    [ADD] Crear asset

---
+++ LiveServer/src/azure/streamingLocator/create.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const router = express.Router()
+const response = require('../../network/response')
+const axios = require('axios')
+const qs = require('qs')
+const config = require('../../../config')
+
+const createAsset = async (info) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'put',
+      url: `https://management.azure.com/subscriptions/${config.azureSubscriptionId}/resourceGroups/${config.azureResourceGroupName}/providers/Microsoft.Media/mediaServices/${config.azureAccountName}/streamingLocators/${info.userID}StreamingLocator?api-version=2018-07-01`,
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${info.token}`,
+      },
+      data: {
+        properties: {
+          streamingPolicyName: 'streamingPolicy3',
+          assetName: `${info.userID}Asset`,
+          contentKeys: [],
+          filters: [],
+        },
+      },
+    })
+      .then((response) => {
+        console.log('Streaming locator created')
+        resolve(response)
+      })
+      .catch((err) => {
+        console.error('Cant create streaming locator')
+        reject(err)
+      })
+  })
+}
+
+module.exports = createAsset
