ba84ff97d02a0b234bff751bedfc7b4a90667d40
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-10-06 02:05:38 -0500

    [ADD]

---
+++ LiveServer/src/azure/liveOutput/get.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+const config = require('../../../config')
+
+const getLiveEvent = ({ token, userId }) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'get',
+      url: `https://management.azure.com/subscriptions/${config.azureSubscriptionId}/resourceGroups/${config.azureResourceGroupName}/providers/Microsoft.Media/mediaservices/${config.azureAccountName}/liveEvents/${userId}}/liveOutputs/${userId}Output?api-version=2018-07-01`,
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then((response) => {
+        console.log('Live event exist')
+        resolve(response)
+      })
+      .catch((error) => {
+        console.error('Live event does not exist')
+        reject(error)
+      })
+  })
+}
+
+module.exports = getLiveEvent
