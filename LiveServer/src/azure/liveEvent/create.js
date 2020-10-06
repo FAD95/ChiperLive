2fa3d4c0636249e195f6f3f2061b26cc3a9020bd
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-10-06 02:10:37 -0500

    linted

--- LiveServer/src/azure/liveEvent/create.js
+++ LiveServer/src/azure/liveEvent/create.js
@@ -1,20 +1,19 @@
-const express = require('express')
 const axios = require('axios')
 const config = require('../../../config')
 
-const createLiveEvent = (info) => {
+const createLiveEvent = ({ token, userId, liveName }) => {
   return new Promise((resolve, reject) => {
     axios({
       method: 'put',
-      url: `https://management.azure.com/subscriptions/${config.azureSubscriptionId}/resourceGroups/${config.azureResourceGroupName}/providers/Microsoft.Media/mediaservices/${config.azureAccountName}/liveEvents/${info.userID}?api-version=2018-07-01&autoStart=false`,
+      url: `https://management.azure.com/subscriptions/${config.azureSubscriptionId}/resourceGroups/${config.azureResourceGroupName}/providers/Microsoft.Media/mediaservices/${config.azureAccountName}/liveEvents/${userId}?api-version=2018-07-01&autoStart=false`,
       headers: {
         Accept: 'application/json',
-        Authorization: `Bearer ${info.token}`,
+        Authorization: `Bearer ${token}`
       },
       data: {
-        location: 'southcentralus',
+        location: config.azureRegion,
         properties: {
-          description: info.liveName,
+          description: liveName,
           vanityUrl: true,
           streamOptions: ['LowLatency'],
           input: {
@@ -27,11 +26,11 @@ const createLiveEvent = (info) => {
                   {
                     name: 'AllowAll',
                     address: '0.0.0.0',
-                    subnetPrefixLength: 0,
-                  },
-                ],
-              },
-            },
+                    subnetPrefixLength: 0
+                  }
+                ]
+              }
+            }
           },
           preview: {
             accessControl: {
@@ -40,18 +39,18 @@ const createLiveEvent = (info) => {
                   {
                     name: 'AllowAll',
                     address: '0.0.0.0',
-                    subnetPrefixLength: 0,
-                  },
-                ],
-              },
-            },
+                    subnetPrefixLength: 0
+                  }
+                ]
+              }
+            }
           },
           encoding: {
             encodingType: 'Standard',
-            presetName: '',
-          },
-        },
-      },
+            presetName: ''
+          }
+        }
+      }
     })
       .then((response) => {
         console.log('Live event created')
