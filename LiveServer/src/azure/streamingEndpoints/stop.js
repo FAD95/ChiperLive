f64b74e021fe36023434912bab0b4f7bd50838f9
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-10-10 20:54:53 -0500

    [ADD] para parar un streaming endpoint

---
+++ LiveServer/src/azure/streamingEndpoints/stop.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const config = require('../../../config')
+
+const stopStreamingEndpoint = ({ azureToken, userId }) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'POST',
+      
+      url: `https://management.azure.com/subscriptions/${config.azureSubscriptionId}/resourceGroups/${config.azureResourceGroupName}/providers/Microsoft.Media/mediaservices/${config.azureAccountName}/streamingEndpoints/${userId}StreamingEndpoint/stop?api-version=${config.azureApiVersion}`,
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${azureToken}`,
+        Connection: 'keep-alive'
+      }
+    })
+      .then((response) => {
+        console.log('Streaming Endpoint deleted')
+        resolve(response)
+      })
+      .catch((error) => {
+        console.error('Cant delete Streaming Endpoint')
+        reject(error)
+      })
+  })
+}
+
+module.exports = stopStreamingEndpoint
