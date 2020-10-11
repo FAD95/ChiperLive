0005059a8f4fee807d1ec15cebaf4be13280cf2c
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-10-10 20:58:44 -0500

    [FIX] token por azureToken

--- LiveServer/src/azure/mediaAsset/create.js
+++ LiveServer/src/azure/mediaAsset/create.js
@@ -1,17 +1,17 @@
 const axios = require('axios')
 const config = require('../../../config')
 
-const createMediaAsset = async ({ token, liveName, userId }) => {
+const createMediaAsset = async ({ azureToken, liveName, userId }) => {
   const d = new Date()
   const date = d.toLocaleDateString() + '-' + d.getTime()
   const assetName = userId + '-' + date
   return new Promise((resolve, reject) => {
     axios({
       method: 'put',
-      url: `https://management.azure.com/subscriptions/${config.azureSubscriptionId}/resourceGroups/${config.azureResourceGroupName}/providers/Microsoft.Media/mediaServices/${config.azureAccountName}/assets/${assetName}?api-version=2018-07-01`,
+      url: `https://management.azure.com/subscriptions/${config.azureSubscriptionId}/resourceGroups/${config.azureResourceGroupName}/providers/Microsoft.Media/mediaServices/${config.azureAccountName}/assets/${assetName}?api-version=${config.azureApiVersion}`,
       headers: {
         Accept: 'application/json',
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${azureToken}`
       },
       data: {
         properties: {
