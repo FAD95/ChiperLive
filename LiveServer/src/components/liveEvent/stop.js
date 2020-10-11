feaba2d098bd7ad3567e421d8789c1dfddb8f742
FAD95 <36573399+FAD95@users.noreply.github.com>
2020-10-10 20:50:42 -0500

    [ADD] en point para parar el live event

---
+++ LiveServer/src/components/liveEvent/stop.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const getToken = require('../../azure/login')
+const stop = require('../../azure/liveEvent/stop')
+
+router.post('/', async (req, res, next) => {
+  const { userId } = req.body
+
+  try {
+    const azureToken = await getToken()
+    await stop({azureToken,userId})
+    res.status(200).send('Live Event Stoped')
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+module.exports = router
